c5f834259afc92cc70fd90d1465433bcc426f64e
Quillan Ogle <quillan131518@gmail.com>
2022-01-24 17:13:57 -0500

    added router for pages, added logo and browser tab image, added app bar, created page 2, all temp, design is ugly lol

---
+++ src/Pages/Home.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from '../logo.svg'
+import '../App.css';
+import ResponsiveAppBar from '../Components/ResponsiveAppBar';
+import BasicAppBar from '../Components/BasicAppBar'
+
+const Home = () => {
+
+	return (
+		// <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Welcome to the Local Softball Association Homepage
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       More information to come, also Kayden, you are still a little bitch.
+    //     </a>
+    //   </header>
+    // </div>
+    //<ResponsiveAppBar></ResponsiveAppBar>
+    <>
+    <BasicAppBar></BasicAppBar>
+
+    <div>Stuff Goes Here</div>
+    <div> and more stuff here</div>
+</>
+	);
+};
+
+export default Home;
